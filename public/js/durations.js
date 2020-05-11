3b7efc537f2b8b035d6d4155486e6791ec59cc55
Brice Boutet <boutetlb@gmail.com>
2020-05-11 17:49:29 -0400

    started database and driver api construction

--- public/js/durations.js
+++ public/js/durations.js
@@ -1,37 +1,73 @@
 const axios = require("axios");
 
 const time = {
-
     async getDuration(origin, destination, waypoints) {
         try {
-            //const origin = "L7A 0W6";
-            //const destination = "L4W 2S5";
-            //const waypointsArray = ["L7A 3P6", "L6T 4P8"]
-            // let waypoints = ""
-            // for (let i = 0; i < waypointsArray.length; i++) { waypoints = waypoints + "|" + waypointsArray[i] };
-            let durationSeconds = 0;
+            let durationSec = 0;
             const { data } = await axios.get(
                 `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&waypoints=${waypoints}&region=ca&key=AIzaSyDXes9rc5Lm0f5N-D5M1lZO3ShcRCJPWk8`
             );
             const legs = data.routes[0].legs;
             for (let i = 0; i < legs.length; i++) {
-                durationSeconds += legs[i].duration.value;
+                durationSec += legs[i].duration.value;
             }
-            console.log(durationSeconds);
-
-            return durationSeconds;
+            console.log(durationSec);
+            return durationSec;
         } catch (err) {
             console.log(err);
         }
     },
 };
+
+const origin = "L7A 0W6";
+const destination = "L4W 2S5";
+const noWaypoint = ""
+const waypoints = "L7A 3P6|L6T 4P8";
+let directDurationSec, directDuration;
+let poolDurationSec, poolDuration;
+let timeDifferenceSec, timeDifference;
+
 async function getD() {
-    const origin = "L7A 0W6";
-    const destination = "L4W 2S5";
-    const waypoints = "L7A 3P6|L6T 4P8";
-    var dur = await time.getDuration(origin, destination, waypoints)
-    console.log(dur)
+    // Time in seconds
+    directDurationSec = await time.getDuration(origin, destination, noWaypoint);
+    poolDurationSec = await time.getDuration(origin, destination, waypoints);
+    timeDifferenceSec = await poolDurationSec - directDurationSec;
+    //-----------------------------------------------------------------
+    // Time in hours and minutes
+    directDuration = await timeConversion(directDurationSec);
+    poolDuration = await timeConversion(poolDurationSec);
+    timeDifference = await timeConversion(timeDifferenceSec);
 }
 getD()
+
+async function timeConversion(time) {
+    // Converting seconds in hours and minutes
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const duration = hours + " hours " + minutes + " minutes"
+    console.log(duration)
+    return duration;
+}
+
+
+// Calling the upsertDriver function and passing in the value of the name input
+upsertDriver({
+    name: "",
+    homeAddress: origin,
+    workAddress: destination,
+    email: "",
+    waypoints: waypoints,
+    directDuration: directDuration,
+    poolDuraction: poolDuration,
+    timeDifference: timeDifference,
+    passagenrNumber: 0,
+});
+
+// A function for creating an driver. Calls getDrivers upon completion
+function upsertDriver(driverData) {
+    // $.post("/api/drivers", driverData)
+    //     .then(getDrivers);
+}
+
 
 module.exports = time;
