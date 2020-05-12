0ea41d5d2070409f2b02edf36ecf9fb86d909beb
Brice Boutet <boutetlb@gmail.com>
2020-05-12 00:27:51 -0400

    Driver API construction functionnal

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1742,229 +1742,73 @@ module.exports = {
 },{"./helpers/bind":18}],28:[function(require,module,exports){
 const axios = require("axios");
 
-app.use(cors())
 const time = {
     async getDuration(origin, destination, waypoints) {
         try {
-            let durationSeconds = 0;
+            let durationSec = 0;
             const { data } = await axios.get(
                 `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&waypoints=${waypoints}&region=ca&key=AIzaSyDXes9rc5Lm0f5N-D5M1lZO3ShcRCJPWk8`
             );
             const legs = data.routes[0].legs;
             for (let i = 0; i < legs.length; i++) {
-                durationSeconds += legs[i].duration.value;
+                durationSec += legs[i].duration.value;
             }
-            console.log(durationSeconds);
-            return durationSeconds;
+            console.log(durationSec);
+            return durationSec;
         } catch (err) {
             console.log(err);
         }
     },
 };
+
+const origin = "L7A 0W6";
+const destination = "L4W 2S5";
+const noWaypoint = ""
+const waypoints = "L7A 3P6|L6T 4P8";
+let directDurationSec, directDuration;
+let poolDurationSec, poolDuration;
+let timeDifferenceSec, timeDifference;
+
 async function getD() {
-    const origin = "L7A 0W6";
-    const destination = "L4W 2S5";
-    const waypoints = "L7A 3P6|L6T 4P8";
-    var dur = await time.getDuration(origin, destination, waypoints)
-    console.log(dur)
+    // Time in seconds
+    directDurationSec = await time.getDuration(origin, destination, noWaypoint);
+    poolDurationSec = await time.getDuration(origin, destination, waypoints);
+    timeDifferenceSec = await poolDurationSec - directDurationSec;
+    //-----------------------------------------------------------------
+    // Time in hours and minutes
+    directDuration = await timeConversion(directDurationSec);
+    poolDuration = await timeConversion(poolDurationSec);
+    timeDifference = await timeConversion(timeDifferenceSec);
 }
 getD()
 
+async function timeConversion(time) {
+    // Converting seconds in hours and minutes
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const duration = hours + " hours " + minutes + " minutes"
+    console.log(duration)
+    return duration;
+}
+
+
+// Calling the upsertDriver function and passing in the value of the name input
+upsertDriver({
+    name: "",
+    homeAddress: origin,
+    workAddress: destination,
+    email: "",
+    waypoints: waypoints,
+    directDuration: directDuration,
+    poolDuraction: poolDuration,
+    timeDifference: timeDifference,
+    passagenrNumber: 0,
+});
+
+// A function for creating an driver. Calls getDrivers upon completion
+function upsertDriver(driverData) {
+    $.post("/api/drivers", driverData)
+        .then(getDrivers);
+}
 module.exports = time;
-},{"axios":2}],29:[function(require,module,exports){
-// Initialise some variables
-var directionsService = new google.maps.DirectionsService();
-var num, map, data, bounds;
-var requestArray = [],
-    renderArray = [];
-
-// A JSON Array containing some people/routes and the destinations/stops
-var jsonArray = {
-    "driver": ["L7A 0W6", "L4W 2S5"],
-    "passenger1": ["L7A 3P6", "L6T 4P8"]
-}
-
-
-var numWaypoints = jsonArray.passenger1.length;
-let waypoints = ""
-for (var i = 0; i < numWaypoints; i++) {
-    if (i !== 0) { waypoints = waypoints + "|" + jsonArray.passenger1[i] } else { waypoints = +jsonArray.passenger1[i] }
-};
-var time = require("./durations");
-async function durations() {
-    // calculate route duration
-    var directDuration = await time.getDuration(jsonArray.driver[0], jsonArray.driver[1]);
-    var carPoolDuration = await time.getDuration(jsonArray.driver[0], jsonArray.driver[1], waypoints);
-    var timeDifference = carPoolDuration[0] - directDuration[0];
-    console.log(numWaypoints)
-    console.log(waypoints)
-    console.log(directDuration)
-    console.log(carPoolDuration)
-}
-durations()
-
-
-// Standard Colours for navigation polylines
-var colourArray = ['maroon', 'purple', 'aqua', 'red', 'green', 'silver', 'olive', 'blue', 'yellow', 'teal'];
-
-// Let's make an array of requests which will become individual polylines on the map.
-function generateRequests() {
-
-    requestArray = [];
-
-    for (var route in jsonArray) {
-        // This now deals with one of the people / routes
-
-        // Somewhere to store the wayoints
-        var waypts = [];
-
-        // 'start' and 'finish' will be the routes origin and destination
-        var start, finish
-
-        // lastpoint is used to ensure that duplicate waypoints are stripped
-        var lastpoint
-
-        data = jsonArray[route]
-
-        limit = data.length
-        for (var waypoint = 0; waypoint < limit; waypoint++) {
-            if (data[waypoint] === lastpoint) {
-                // Duplicate of of the last waypoint - don't bother
-                continue;
-            }
-
-            // Prepare the lastpoint for the next loop
-            lastpoint = data[waypoint]
-
-            // Add this to waypoint to the array for making the request
-            waypts.push({
-                location: data[waypoint],
-                stopover: true
-            });
-        }
-
-        // Grab the first waypoint for the 'start' location
-        start = (waypts.shift()).location;
-        // Grab the last waypoint for use as a 'finish' location
-        finish = waypts.pop();
-        if (finish === undefined) {
-            // Unless there was no finish location for some reason?
-            finish = start;
-        } else {
-            finish = finish.location;
-        }
-
-        // Let's create the Google Maps request object
-        var request = {
-            origin: start,
-            destination: finish,
-            waypoints: waypts,
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-
-        // and save it in our requestArray
-        requestArray.push({
-            "route": route,
-            "request": request
-        });
-    }
-
-    processRequests();
-}
-
-function processRequests() {
-
-    // Counter to track request submission and process one at a time;
-    var i = 0;
-
-    // Used to submit the request 'i'
-    function submitRequest() {
-        directionsService.route(requestArray[i].request, directionResults);
-    }
-
-    // Used as callback for the above request for current 'i'
-    function directionResults(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-
-            // Create a unique DirectionsRenderer 'i'
-            renderArray[i] = new google.maps.DirectionsRenderer();
-            renderArray[i].setMap(map);
-
-
-
-            // Some unique options from the colorArray so we can see the routes
-            renderArray[i].setOptions({
-                preserveViewport: true,
-                suppressInfoWindows: true,
-                polylineOptions: {
-                    strokeWeight: 4,
-                    strokeOpacity: 0.8,
-                    strokeColor: colourArray[i]
-                },
-                markerOptions: {
-                    icon: {
-                        path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                        scale: 3,
-                        strokeColor: colourArray[i]
-                    }
-                }
-            });
-
-            // Use this new renderer with the result
-            renderArray[i].setDirections(result);
-            // and start the next request
-            nextRequest();
-
-            bounds.union(result.routes[0].bounds);
-            // zoom and center the map to show all the routes
-            map.fitBounds(bounds);
-
-
-            //Adding some html
-            if (i > 0) {
-                const checkbox = document.querySelector('form');
-                ("<input type=\"checkbox\"id =\"vehicle1\"name = \"vehicle1\"value = \" Bike\"> ")
-            }
-
-        }
-
-    }
-
-    function nextRequest() {
-        // Increase the counter
-        i++;
-        // Make sure we are still waiting for a request
-        if (i >= requestArray.length) {
-            // No more to do
-            return;
-        }
-        // Submit another request
-        submitRequest();
-    }
-
-    // This request is just to kick start the whole process
-    submitRequest();
-}
-
-// Called Onload
-function init() {
-
-    // Some basic map setup (from the API docs)
-    var mapOptions = {
-        //center: new google.maps.LatLng(43.651070, -79.347015),
-        //zoom: 10,
-        mapTypeControl: false,
-        streetViewControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    // Start the request making
-    generateRequests()
-    bounds = new google.maps.LatLngBounds();
-}
-
-// Get the ball rolling and trigger our init() on 'load'
-google.maps.event.addDomListener(window, 'load', init);
-},{"./durations":28}]},{},[29]);
+},{"axios":2}]},{},[28]);
--- routes/driver-api-routes.js
+++ routes/driver-api-routes.js
@@ -1,16 +1,16 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    app.get("/api/drivers", function(req, res) {
-        // Here we add an "include" property to our options in our findAll query
-        // We set the value to an array of the models we want to include in a left outer join
-        // In this case, just db.Passenger
-        db.Driver.findAll({
-            include: [db.Passenger]
-        }).then(function(dbDriver) {
-            res.json(dbDriver);
-        });
-    });
+    // app.get("/api/drivers", function(req, res) {
+    //     // Here we add an "include" property to our options in our findAll query
+    //     // We set the value to an array of the models we want to include in a left outer join
+    //     // In this case, just db.Passenger
+    //     db.Driver.findAll({
+    //         include: [db.Passenger]
+    //     }).then(function(dbDriver) {
+    //         res.json(dbDriver);
+    //     });
+    // });
 
     app.get("/api/drivers/:id", function(req, res) {
         // Here we add an "include" property to our options in our findOne query
@@ -25,12 +25,97 @@ module.exports = function(app) {
             res.json(dbDriver);
         });
     });
+    //==========================
 
-    app.post("/api/drivers", function(req, res) {
-        db.Driver.create(req.body).then(function(dbDriver) {
-            res.json(dbDriver);
-        });
-    });
+    //Driver informations
+
+    const axios = require("axios");
+
+    const time = {
+        async getDuration(origin, destination, waypoints) {
+            try {
+                let durationSec = 0;
+                const { data } = await axios.get(
+                    `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&waypoints=${waypoints}&region=ca&key=AIzaSyDXes9rc5Lm0f5N-D5M1lZO3ShcRCJPWk8`
+                );
+                const legs = data.routes[0].legs;
+                for (let i = 0; i < legs.length; i++) {
+                    durationSec += legs[i].duration.value;
+                }
+                console.log(durationSec);
+                return durationSec;
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    };
+
+    const origin = "L7A 0W6";
+    const destination = "L4W 2S5";
+    const noWaypoint = ""
+    const waypoints = "L7A 3P6|L6T 4P8";
+    let directDurationSec, directDuration;
+    let poolDurationSec, poolDuration;
+    let timeDifferenceSec, timeDifference;
+    let driverData;
+
+    async function getD() {
+        // Time in seconds
+        directDurationSec = await time.getDuration(origin, destination, noWaypoint);
+        poolDurationSec = await time.getDuration(origin, destination, waypoints);
+        timeDifferenceSec = await poolDurationSec - directDurationSec;
+        //-----------------------------------------------------------------
+        // Time in hours and minutes
+        directDuration = await timeConversion(directDurationSec);
+        poolDuration = await timeConversion(poolDurationSec);
+        timeDifference = await timeConversion(timeDifferenceSec);
+
+        driverData = [{
+                name: "",
+                homeAddress: origin,
+                workAddress: destination,
+                email: "",
+                waypoints: waypoints,
+                directDuration: directDuration,
+                poolDuraction: poolDuration,
+                timeDifference: timeDifference,
+                passagenrNumber: 0,
+            }]
+            // Calling the upsertDriver function and passing in the DriverData objet
+            //===========================================================================
+        upsertDriver(driverData);
+    }
+    getD()
+    async function timeConversion(time) {
+        // Converting seconds in hours and minutes
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) / 60);
+        const duration = hours + " hours " + minutes + " minutes"
+        console.log(duration)
+        return duration;
+    }
+
+    // A function for creating an driver. Calls getDrivers upon completion
+    function upsertDriver(driverData) {
+        // $.post("/api/drivers", driverData)
+        //     .then(getDrivers);
+        console.log(driverData)
+        app.get('/api/drivers', (req, res) => {
+            res.send(driverData)
+        })
+
+    }
+    //===============================================
+
+
+
+
+
+    // app.post("/api/drivers", function(req, res) {
+    //     db.Driver.create(req.body).then(function(dbDriver) {
+    //         res.json(dbDriver);
+    //     });
+    // });
 
     app.delete("/api/drivers/:id", function(req, res) {
         db.Driver.destroy({
