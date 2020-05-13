d44ebb699de57639eda137ba0f6a1a9f44b43c43
Brice Boutet <boutetlb@gmail.com>
2020-05-13 16:11:45 -0400

    Frontend html update, select input for user type

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -33,30 +33,15 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/routesMap.html"));
     });
 
-
-
-    //OLD ROUTES BELOW (to be deleted)
-    //---------------------------------------------------------------
-
-    // index route loads view.html
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/blog.html"));
+    // authors route loads routesMap.html
+    app.get("/passenger-map", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/routesMapPassenger.html"));
     });
 
-    // cms route loads cms.html
-    app.get("/cms", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/cms.html"));
-    });
 
-    // blog route loads blog.html
-    app.get("/blog", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/blog.html"));
+    // authors route loads routesMap.html
+    app.get("/continue", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/continue.html"));
     });
-
-    // authors route loads author-manager.html
-    app.get("/authors", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-    });
-
 
 };
