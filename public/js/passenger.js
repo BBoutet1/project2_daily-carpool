328aa23013cfb23ea53f4594585ab5514ecf76bf
Brice Boutet <boutetlb@gmail.com>
2020-05-13 20:16:46 -0400

    Created passenger object and database

--- public/js/passenger.js
+++ public/js/passenger.js
@@ -1,108 +1,102 @@
 $(document).ready(function() {
-    // Getting references to the name input and driver container, as well as the table body
-    var driverName = "";
-    // var driverEmail = $("#email");
-    var driverOrigin = "";
-    var driverDestination = "";
+    // Getting references to the name input and passenger container, as well as the table body
+    var passengerName = "";
+    // var passengerEmail = $("#email");
+    var passengerOrigin = "";
+    var passengerDestination = "";
 
     // Adding event listeners to the form to create a new object, and the button to delete
-    // an Driver
-    $("#passenger-form").on("submit", handleDriverFormSubmit);
-    //$(document).on("click", ".delete-driver", handleDeleteButtonPress);
+    // an Passenger
+    $("#passenger-form").on("submit", handlePassengerFormSubmit);
+    //$(document).on("click", ".delete-passenger", handleDeleteButtonPress);
 
-    // Getting the initial list of Drivers
-    getDrivers();
+    // Getting the initial list of Passengers
+    getPassengers();
 
-    // A function to handle what happens when the form is submitted to create a new Driver
-    function handleDriverFormSubmit(event) {
+    // A function to handle what happens when the form is submitted to create a new Passenger
+    function handlePassengerFormSubmit(event) {
         event.preventDefault();
-        console.log("toto")
-            // Getting references to the name input and driver container, as well as the table body
-        driverName = $("#name");
-        // var driverEmail = $("#email");
-        driverOrigin = $("#origin");
-        driverDestination = $("#destination");
+        // Getting references to the name input and passenger container, as well as the table body
+        passengerName = $("#name");
+        // var passengerEmail = $("#email");
+        passengerOrigin = $("#origin");
+        passengerDestination = $("#destination");
         // Don't do anything if the name fields hasn't been filled out
-        if (!driverName.val().trim() || !driverOrigin.val().trim() || !driverDestination.val().trim()) {
+        if (!passengerName.val().trim() || !passengerOrigin.val().trim() || !passengerDestination.val().trim()) {
             return;
         }
-        // Calling the upsertDriver function and passing in values of driver parameters
-        upsertDriver({
-            name: driverName.val().trim(),
-            homeAddress: driverOrigin.val().trim(),
-            workAddress: driverDestination.val().trim(),
-            email: "",
+        // Calling the upsertPassenger function and passing in values of passenger parameters
+        upsertPassenger({
+            name: passengerName.val().trim(),
+            homeAddress: passengerOrigin.val().trim(),
+            workAddress: passengerDestination.val().trim(),
             waypoints: "",
-            directDuration: 0,
-            poolDuraction: 0,
-            timeDifference: 0,
-            passagenrNumber: 0,
         });
     }
 
-    // A function for creating an driver. Calls getDrivers upon completion
-    function upsertDriver(driverData) {
-        console.log(driverData)
-        $.post("/api/passengers", driverData)
-            .then(getDrivers);
+    // A function for creating an passenger. Calls getPassengers upon completion
+    function upsertPassenger(passengerData) {
+        console.log(passengerData)
+        $.post("/api/passengers", passengerData)
+            .then(getPassengers);
     }
 
-    // Function for creating a new list row for drivers
-    function createDriverRow(driverData) {
+    // Function for creating a new list row for passengers
+    function createPassengerRow(passengerData) {
         var newTr = $("<tr>");
-        newTr.data("driver", driverData);
-        newTr.append("<td>" + driverData.name + "</td>");
-        if (driverData.Posts) {
-            newTr.append("<td> " + driverData.Posts.length + "</td>");
+        newTr.data("passenger", passengerData);
+        newTr.append("<td>" + passengerData.name + "</td>");
+        if (passengerData.Posts) {
+            newTr.append("<td> " + passengerData.Posts.length + "</td>");
         } else {
             newTr.append("<td>0</td>");
         }
-        newTr.append("<td><a href='/blog?driver_id=" + driverData.id + "'>Go to Posts</a></td>");
-        newTr.append("<td><a href='/cms?driver_id=" + driverData.id + "'>Create a Post</a></td>");
-        newTr.append("<td><a style='cursor:pointer;color:red' class='delete-driver'>Delete Driver</a></td>");
+        newTr.append("<td><a href='/blog?passenger_id=" + passengerData.id + "'>Go to Posts</a></td>");
+        newTr.append("<td><a href='/cms?passenger_id=" + passengerData.id + "'>Create a Post</a></td>");
+        newTr.append("<td><a style='cursor:pointer;color:red' class='delete-passenger'>Delete Passenger</a></td>");
         return newTr;
     }
 
-    // Function for retrieving drivers and getting them ready to be rendered to the page
-    function getDrivers() {
-        $.get("/api/drivers", function(data) {
+    // Function for retrieving passengers and getting them ready to be rendered to the page
+    function getPassengers() {
+        $.get("/api/passengers", function(data) {
             var rowsToAdd = [];
             for (var i = 0; i < data.length; i++) {
-                rowsToAdd.push(createDriverRow(data[i]));
+                rowsToAdd.push(createPassengerRow(data[i]));
             }
-            // renderDriverList(rowsToAdd);
-            driverName.val("");
+            // renderPassengerList(rowsToAdd);
+            passengerName.val("");
         });
     }
 
-    // A function for rendering the list of drivers to the page
-    function renderDriverList(rows) {
-        driverList.children().not(":last").remove();
-        driverContainer.children(".alert").remove();
+    // A function for rendering the list of passengers to the page
+    function renderPassengerList(rows) {
+        passengerList.children().not(":last").remove();
+        passengerContainer.children(".alert").remove();
         if (rows.length) {
             console.log(rows);
-            driverList.prepend(rows);
+            passengerList.prepend(rows);
         } else {
             renderEmpty();
         }
     }
 
-    // Function for handling what to render when there are no drivers
+    // Function for handling what to render when there are no passengers
     function renderEmpty() {
         var alertDiv = $("<div>");
         alertDiv.addClass("alert alert-danger");
-        alertDiv.text("You must create an Driver before you can create a Post.");
-        driverContainer.append(alertDiv);
+        alertDiv.text("You must create an Passenger before you can create a Post.");
+        passengerContainer.append(alertDiv);
     }
 
     // Function for handling what happens when the delete button is pressed
     // function handleDeleteButtonPress() {
-    //     var listItemData = $(this).parent("td").parent("tr").data("driver");
+    //     var listItemData = $(this).parent("td").parent("tr").data("passenger");
     //     var id = listItemData.id;
     //     $.ajax({
     //             method: "DELETE",
-    //             url: "/api/drivers/" + id
+    //             url: "/api/passengers/" + id
     //         })
-    //         .then(getDrivers);
+    //         .then(getPassengers);
     // }
 });
